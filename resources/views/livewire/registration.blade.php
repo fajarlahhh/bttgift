a2cad2d35e2ea9d31f404d835d64017bf29447b4
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-06-17 15:41:44 +0800

    register

--- resources/views/livewire/registration.blade.php
+++ resources/views/livewire/registration.blade.php
@@ -2,8 +2,41 @@
     <div class="block xl:grid grid-cols-2 gap-4">
         <!-- BEGIN: Login Info -->
         <div class="hidden xl:flex flex-col min-h-screen">
-            <div class="my-auto">
-                <img alt="{{ config('app.name') }}" class="-intro-x w-1/2 -mt-16" src="/images/logo.svg">
+            <a href="" class="-intro-x flex items-center pt-5">
+                <img alt="{{ config('app.name') }}" class="w-10" src="/images/favicon.svg">
+                <span class="text-white text-lg ml-3">
+                    Diamond <span class="font-medium">Glow</span>
+                    <hr class="text-white">
+                </span>
+            </a>
+            <div class="-intro-x grid grid-cols-12 gap-6 mt-5 mt-5 text-white mr-10 pr-10">
+                <div class="intro-y col-span-12 lg:col-span-12 ">
+                    <strong>Contract :</strong>
+                    <p><i data-feather="check"></i> $50 Get 150%</p>
+                    <p><i data-feather="check"></i> $100 Get 175%</p>
+                    <p><i data-feather="check"></i> $200 Get 200%</p>
+                    <p><i data-feather="check"></i> $500 Get 225%</p>
+                    <p><i data-feather="check"></i> $1000 Get 250%</p>
+                    <p><i data-feather="check"></i> $2000 Get 275%</p>
+                    <p><i data-feather="check"></i> $5000 Get 300%</p>
+                    <p><i data-feather="check"></i> $10,000 Get 350%</p>
+                    <br>
+                    <strong>Bonuses :</strong>
+                    <p><i data-feather="check"></i> Daily Passive 0.25% - UNLIMITED</p>
+                    <p><i data-feather="check"></i> Referral Active 10%</p>
+                    <br>
+                    <strong>Pairing :</strong>
+                    <p><i data-feather="check"></i> Level 1 = 5%</p>
+                    <p><i data-feather="check"></i> Level 2 = 4%</p>
+                    <p><i data-feather="check"></i> Level 3 = 3%</p>
+                    <p><i data-feather="check"></i> Level 4 - 10 = 2%</p>
+                    <br>
+                    <strong>Achievement :</strong>
+                    <p><i data-feather="check"></i> Min. Turnover $ 10,000 GET $ 200</p>
+                    <p><i data-feather="check"></i> Min. Turnover $ 50,000 GET $ 1,000</p>
+                    <p><i data-feather="check"></i> Min. Turnover $ 100,000 GET $ 2,000</p>
+                    <p><i data-feather="check"></i> Min. Turnover $ 500,000 GET $ 10,000</p>
+                </div>
             </div>
         </div>
         <!-- END: Login Info -->
@@ -11,10 +44,40 @@
         <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
             <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-dark-1 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                 <form wire:submit.prevent="submit">
-                    <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
+                    <div class="intro-y mt-2 xl:hidden">
+                        <div class="alert alert-dark show">
+                            <h3 class="text-2xl text-center">Marketing Plan</h3>
+                            <hr class="mt-2 mb-2">
+                            <strong>Contract :</strong>
+                            <p><i data-feather="check"></i> $50 Get 150%</p>
+                            <p><i data-feather="check"></i> $100 Get 175%</p>
+                            <p><i data-feather="check"></i> $200 Get 200%</p>
+                            <p><i data-feather="check"></i> $500 Get 225%</p>
+                            <p><i data-feather="check"></i> $1000 Get 250%</p>
+                            <p><i data-feather="check"></i> $2000 Get 275%</p>
+                            <p><i data-feather="check"></i> $5000 Get 300%</p>
+                            <p><i data-feather="check"></i> $10,000 Get 350%</p>
+                            <br>
+                            <strong>Bonuses :</strong>
+                            <p><i data-feather="check"></i> Daily Passive 0.25% - UNLIMITED</p>
+                            <p><i data-feather="check"></i> Referral Active 10%</p>
+                            <br>
+                            <strong>Pairing :</strong>
+                            <p><i data-feather="check"></i> Level 1 = 5%</p>
+                            <p><i data-feather="check"></i> Level 2 = 4%</p>
+                            <p><i data-feather="check"></i> Level 3 = 3%</p>
+                            <p><i data-feather="check"></i> Level 4 - 10 = 2%</p>
+                            <br>
+                            <strong>Achievement :</strong>
+                            <p><i data-feather="check"></i> Min. Turnover $ 10,000 GET $ 200</p>
+                            <p><i data-feather="check"></i> Min. Turnover $ 50,000 GET $ 1,000</p>
+                            <p><i data-feather="check"></i> Min. Turnover $ 100,000 GET $ 2,000</p>
+                            <p><i data-feather="check"></i> Min. Turnover $ 500,000 GET $ 10,000</p>
+                        </div>
+                    </div>
+                    <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left mt-5">
                         Sign Up
                     </h2>
-                    <div class="intro-x mt-2 text-gray-500 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your accounts in one place</div>
                     <div class="intro-x mt-8">
                         <input type="text" class="intro-x login__input form-control py-3 px-4 border-gray-300 block" wire:model.defer="username" placeholder="Username" required>
                         <input type="text" class="intro-x login__input form-control py-3 px-4 border-gray-300 block mt-4" placeholder="Full Name" wire:model.defer="name" required>
